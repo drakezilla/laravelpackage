cbf16d4b607a2e0192a025dbd199878f0cd89450
drakezilla <dt.drake@gmail.com>
2021-02-28 11:55:02 +0100

    Se crea la asociacion del provider

--- packages/Blog/Frontend/src/Providers/BlogFrontendServiceProvider.php
+++ packages/Blog/Frontend/src/Providers/BlogFrontendServiceProvider.php
@@ -7,11 +7,14 @@ use Illuminate\Support\ServiceProvider;
 class BlogFrontendServiceProvider extends ServiceProvider {
 
     public function boot() {
-        
+        $this->provides(self::class);
+        $this->publishes([
+            __DIR__ . '/../../config/email.php' => config_path('email.php'),
+        ]);
     }
 
     public function register() {
-        $this->app->make('Blog\Frontend\Frontend');
+        
     }
 
 }
